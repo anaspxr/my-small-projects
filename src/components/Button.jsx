6c5b48964fa7f8b6da1ae02c676c4034de4ded2c
ANAS <anaspappadan@gmail.com>
2024-06-08 14:10:56 +0530

    fix bugs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,72 +2,72 @@ import { useState } from "react";
 import ShowResult from "./ShowResult";
 export default function Button() {
   const [finalResult, setFinalResult] = useState(null);
-  const [count, setCount] = useState(0);
   const [result, setResult] = useState(null);
   const [choice, setChoice] = useState(null);
+
   const stonePaperScissor = (val) => {
     setChoice(val);
     switch (Math.ceil(Math.random() * 3)) {
       case 1:
         setResult("Stone");
+        getResult("Stone", val);
         break;
       case 2:
         setResult("Paper");
+        getResult("Paper", val);
         break;
       case 3:
         setResult("Scissor");
+        getResult("Scissor", val);
         break;
       default:
         console.log("error");
     }
-    setCount(count + 1);
-    getResult(result, choice);
   };
+
   const getResult = (a, b) => {
     if (a === b) setFinalResult("Draw");
     else if (a === "Paper") {
       if (b === "Stone") {
-        setFinalResult("Computer Won!! Paper beats stone (idk why)");
+        setFinalResult("Computer Won!! Paper beats stone ");
       } else setFinalResult("You Won!! Scissor cuts through Paper!!");
     } else if (a === "Scissor") {
       if (b === "Stone") {
         setFinalResult("You Won!! Stone breaks Scissor");
       } else setFinalResult("Computer Won!! Scissor cuts through Paper!!");
     } else {
-      if (b === "Paper")
-        setFinalResult("You Won!! Paper beats stone (idk why) ");
+      if (b === "Paper") setFinalResult("You Won!! Paper beats stone  ");
       else setFinalResult("Computer Won!!  Stone breaks Scissor");
     }
   };
   return (
     <div>
-      <p>
-        played {count} {count === 1 ? "time" : "times"}
-      </p>
-      <p className="choice">
-        Pick Your Choice :{" "}
-        <span
-          onClick={() => {
-            stonePaperScissor("Stone");
-          }}
-        >
-          Stone
-        </span>
-        <span
-          onClick={() => {
-            stonePaperScissor("Paper");
-          }}
-        >
-          Paper
-        </span>
-        <span
-          onClick={() => {
-            stonePaperScissor("Scissor");
-          }}
-        >
-          Scissor
-        </span>
-      </p>
+      <div>
+        <p className="choice">
+          Pick Your Choice :{" "}
+          <span
+            onClick={() => {
+              stonePaperScissor("Stone");
+            }}
+          >
+            Stone
+          </span>
+          <span
+            onClick={() => {
+              stonePaperScissor("Paper");
+            }}
+          >
+            Paper
+          </span>
+          <span
+            onClick={() => {
+              stonePaperScissor("Scissor");
+            }}
+          >
+            Scissor
+          </span>
+        </p>
+      </div>
       <ShowResult
         userChoice={choice}
         computerChoice={result}
--- src/components/ShowResult.jsx
+++ src/components/ShowResult.jsx
@@ -1,27 +1,9 @@
-import { useState } from "react";
-
 export default function ShowResult(props) {
-  const [finalResult, setFinalResult] = useState(null);
-  //   if (props.computerChoice === props.userChoice) setFinalResult("Draw");
-  //   else if (props.computerChoice === "Paper") {
-  //     if (props.userChoice === "Stone") {
-  //       setFinalResult("Computer Won!! Paper beats stone (idk why)");
-  //     } else setFinalResult("You Won!! Scissor cuts through Paper!!");
-  //   } else if (props.computerChoice === "Scissor") {
-  //     if (props.userChoice === "Stone") {
-  //       setFinalResult("You Won!! Stone breaks Scissor");
-  //     } else setFinalResult("Computer Won!! Scissor cuts through Paper!!");
-  //   } else {
-  //     if (props.userChoice === "Paper")
-  //       setFinalResult("You Won!! Paper beats stone (idk why) ");
-  //     else setFinalResult("Computer Won!!  Stone breaks Scissor");
-  //   }
-
   return (
     <div>
       <p>Your Choice : {props.userChoice}</p>
       <p>Computer's choice:{props.computerChoice} </p>
-      <p>{props.finalResult}</p>
+      <h1>{props.finalResult}</h1>
     </div>
   );
 }
