b4cc7c7049bf9a7147003e97267126be058d3e91
ANAS <anaspappadan@gmail.com>
2024-06-11 14:08:21 +0530

    added  error handling in quotes

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from "react";
 
 export default function Quotes() {
-  const [quote, setQuote] = useState();
+  const [loading, setLoading] = useState(false);
+  const [quote, setQuote] = useState(null);
+  const [error, setError] = useState(null);
   const fetchQuotes = () => {
-    const randomNum = Math.floor(Math.random() * 30);
+    setLoading(true);
+    setError(null);
     fetch("https://dummyjson.com/quotes")
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error("error");
+        res.json();
+      })
       .then((data) => {
+        const randomNum = Math.floor(Math.random() * data.quotes.length);
         setQuote(data.quotes[randomNum]);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(
+          <p>
+            Failed to fetch from{" "}
+            <a href="https://dummyjson.com/quotes">
+              https://dummyjson.com/quotes
+            </a>
+            Try again later
+          </p>
+        );
+        setLoading(false);
       });
   };
   useEffect(() => {
@@ -19,6 +40,8 @@ export default function Quotes() {
       <button className="fetch-button" onClick={fetchQuotes}>
         Fetch another Quote
       </button>
+      {loading && <span>Loading...</span>}
+      {error}
       <h2>{quote && quote.quote}</h2>
       <h3>{quote && "-" + quote.author}</h3>
     </div>
