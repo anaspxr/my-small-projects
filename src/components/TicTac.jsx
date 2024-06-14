6c063de7833dd46d7eaf59960cb6d6c63f3720b0
ANAS <anaspappadan@gmail.com>
2024-06-14 11:17:37 +0530

    display X or O when clicking columns

--- src/components/TicTac.jsx
+++ src/components/TicTac.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+
 export default function TicTac() {
+  const [play, setPlay] = useState(false);
+  const [turn, setTurn] = useState(true);
+  const [columns, setColumns] = useState([
+    { value: "" },
+    { value: "" },
+    { value: "" },
+    { value: "" },
+    { value: "" },
+    { value: "" },
+    { value: "" },
+    { value: "" },
+    { value: "" },
+  ]);
+  const handleClick = (col) => {
+    if (turn) {
+      setColumns(columns.toSpliced(col, 1, { value: "X" }));
+    } else {
+      setColumns(columns.toSpliced(col, 1, { value: "O" }));
+    }
+    setTurn((prev) => !prev);
+  };
+  useEffect(() => {
+    if (play) {
+    }
+  }, [play]);
   return (
     <div
       className="content-container"
@@ -8,17 +35,38 @@ export default function TicTac() {
         justifyContent: "center",
       }}
     >
-      <button className="tictac-button">Play Tic Tac Toe</button>
+      <button
+        className="tictac-button"
+        onClick={() => {
+          setPlay(!play);
+        }}
+      >
+        Play Tic Tac Toe
+      </button>
+      <div className="tictac-score">
+        <div>
+          <h3>Player 1</h3>
+          {turn && <p>Your Turn..</p>}
+        </div>
+        <div>
+          <h3>Player 2</h3>
+          {!turn && <p>Your Turn..</p>}
+        </div>
+      </div>
       <div className="tictac-container">
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
-        <div className="tictac-cols"></div>
+        {columns.map((col, index) => {
+          return (
+            <div
+              key={index}
+              onClick={() => {
+                handleClick(index);
+              }}
+              className="tictac-cols"
+            >
+              <p className="tictac-text">{col.value}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
