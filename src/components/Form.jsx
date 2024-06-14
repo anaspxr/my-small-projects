5502b6184d0a6f92fb84bf162899f39b8d644115
ANAS <anaspappadan@gmail.com>
2024-06-14 17:57:16 +0530

    complete form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,46 +1,35 @@
 import React, { useEffect, useState } from "react";
 
 function Form() {
+  const localData = JSON.parse(localStorage.getItem("users"));
+  const [currentUsers, setCurrentUsers] = useState(
+    localData ? [...localData] : []
+  );
+  const [loggedUser, setLoggedUser] = useState({});
   const initialValues = { username: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
   const [newUser, setNewUser] = useState(true);
   const [loggedIn, setLoggedIn] = useState(false);
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (newUser) {
-      if (Object.keys(validateSignup(formValues)).length === 0) {
-      } else {
-        setFormErrors(validateSignup(formValues));
-      }
-    } else {
-      setFormErrors(validateLogin(formValues));
-    }
+    newUser ? validateSignup(formValues) : validateLogin(formValues);
   };
-
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit)
-      console.log(formValues);
-  }, [formErrors]);
 
   const validateSignup = (values) => {
     const errors = {};
-
-    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     if (!values.username) errors.username = "Your name is required!!";
-
     if (!values.email) errors.email = "email is required!!";
     else if (!regex.test(values.email)) {
       errors.email = "Enter a valid email";
     }
-
     if (!values.password) errors.password = "password is required!!";
     else if (values.password.length < 8)
       errors.password = "Password must contain at least 8 characters";
@@ -51,15 +40,80 @@ function Form() {
       values.confirmpass !== values.password
     )
       errors.confirmpass = "passwords mismatch!!";
-    return errors;
+
+    if (Object.keys(errors).length === 0) {
+      for (let x of currentUsers) {
+        if (x.email === formValues.email) {
+          errors.email = "This email is already registered";
+        }
+      }
+    }
+
+    if (Object.keys(errors).length === 0) {
+      const newUserObj = {
+        username: formValues.username,
+        email: formValues.email,
+        password: formValues.password,
+      };
+      setCurrentUsers([...currentUsers, newUserObj]);
+      setNewUser(false);
+      setFormValues(initialValues);
+    }
+    setFormErrors(errors);
   };
 
-  const validateLogin = (values) => {};
+  const validateLogin = (values) => {
+    const errors = {};
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!values.email) errors.email = "email is required!!";
+    else if (!regex.test(values.email)) {
+      errors.email = "Enter a valid email";
+    }
+    if (!values.password) errors.password = "enter password";
+
+    if (Object.keys(errors).length === 0) {
+      for (let user of currentUsers) {
+        if (
+          user.email === formValues.email &&
+          user.password === formValues.password
+        ) {
+          setLoggedUser({ username: user.username, email: user.email });
+          setLoggedIn(true);
+          setFormValues(initialValues);
+          break;
+        } else {
+          errors.login = "Invalid Credentials..";
+        }
+      }
+    }
+    setFormErrors(errors);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(currentUsers));
+  }, [currentUsers]);
 
   return (
     <div className="content-container form-container">
       {loggedIn ? (
-        <Profile username={formValues.username} email={formValues.email} />
+        <div className="profile">
+          <p>Logged in Successfully</p>
+          <h1>Your Profile</h1>
+          <p>
+            Name: <span>{loggedUser.username}</span>
+          </p>
+          <p>
+            Email:<span>{loggedUser.email}</span>
+          </p>
+          <p
+            className="form-link"
+            onClick={() => {
+              setLoggedIn(false);
+            }}
+          >
+            Logout
+          </p>
+        </div>
       ) : (
         <form className="form" onSubmit={handleSubmit}>
           <h1>{newUser ? "Sign Up" : "Login"}</h1>
@@ -115,11 +169,13 @@ function Form() {
             </div>
           )}
           {newUser ? <button>Sign Up</button> : <button>Log In</button>}
+          <p className="form-errors">{formErrors.login}</p>
           <div className="bottom-container">
             {newUser ? (
               <p>
                 Already have an account?{" "}
                 <span
+                  className="form-link"
                   onClick={() => {
                     setNewUser(false);
                   }}
@@ -131,6 +187,7 @@ function Form() {
               <p>
                 Don't have an account?{" "}
                 <span
+                  className="form-link"
                   onClick={() => {
                     setNewUser(true);
                   }}
@@ -142,20 +199,6 @@ function Form() {
           </div>
         </form>
       )}
-    </div>
-  );
-}
-
-function Profile(props) {
-  return (
-    <div className="profile">
-      <h1>Your Profile</h1>
-      <p>
-        Name: <span>{props.username}</span>
-      </p>
-      <p>
-        Email:<span>{props.email}</span>
-      </p>
     </div>
   );
 }
