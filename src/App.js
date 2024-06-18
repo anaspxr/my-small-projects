d594757f8a1ef67005d2a0f33252e4eebd171a42
ANAS <anaspappadan@gmail.com>
2024-06-18 10:07:35 +0530

    edit app.js code to show tictactoe as landing page

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ export const CounterContext = React.createContext();
 function App() {
   const [count, setCount] = useState(0);
   const listItems = [
-    "Stone Paper Scissor",
+    "Tic Tac Toe",
     "Quotes",
     "Form",
     "To-Do List",
-    "Tic Tac Toe",
+    "Stone Paper Scissor",
     `Counter (${count})`,
   ];
-  const [showContent, setShowContent] = useState(0);
+  const [showContent, setShowContent] = useState(1);
   const Buttons = listItems.map((content, index) => (
     <button
       key={index}
@@ -36,11 +36,11 @@ function App() {
       <h3>Choose a Project:</h3>
       <CounterContext.Provider value={[setCount]}>
         <div className="buttons-container"> {Buttons}</div>
-        {showContent === 1 && <StonePaperScissor />}
+        {showContent === 1 && <TicTac />}
         {showContent === 2 && <Quotes />}
         {showContent === 3 && <Form />}
         {showContent === 4 && <TodoList />}
-        {showContent === 5 && <TicTac />}
+        {showContent === 5 && <StonePaperScissor />}
         {showContent === 6 && <Counter />}
       </CounterContext.Provider>
     </div>
