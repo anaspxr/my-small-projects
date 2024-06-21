439dd65a17b7980397c6a872601f3cffd88feb8f
ANAS <anaspappadan@gmail.com>
2024-06-21 10:29:36 +0530

    update counter reducer function logic

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import Info from "./Info";
 import { CounterContext } from "../App";
 
@@ -7,44 +7,103 @@ export default function Counter() {
   const initialValue = 0;
 
   const reducer = (state, action) => {
-    return action(state);
+    switch (action.type) {
+      case "increment":
+        return state + action.value;
+      case "decrement":
+        return state - action.value;
+      case "multi":
+        return state * action.value;
+      case "divide":
+        return state / action.value;
+      case "reset":
+        return 0;
+      default:
+        return state;
+    }
   };
-
   const [count, dispatch] = useReducer(reducer, initialValue);
+
+  useEffect(() => {
+    setValue(count);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [count]);
+
   return (
     <div className="content-container">
       <h3 style={{ textAlign: "center" }}>
         A simple counter using useReducer Hook to manage state
       </h3>
       <p style={{ textAlign: "center" }}>Count: {count}</p>
-      <div
-        style={{ display: "flex", justifyContent: "center" }}
-        className="buttons-container"
-      >
+      <div className="counter-buttons">
         <button
           onClick={() => {
-            setValue((value) => value + 1);
-            dispatch((state) => {
-              return state + 1;
-            });
+            dispatch({ type: "increment", value: 1 });
           }}
         >
-          +
+          +1
         </button>
         <button
           onClick={() => {
-            dispatch((state) => {
-              return state - 1;
-            });
+            dispatch({ type: "increment", value: 10 });
           }}
         >
-          -
+          +10
+        </button>
+      </div>
+      <div className="counter-buttons">
+        <button
+          onClick={() => {
+            dispatch({ type: "decrement", value: 1 });
+          }}
+        >
+          -1
+        </button>
+
+        <button
+          onClick={() => {
+            dispatch({ type: "decrement", value: 10 });
+          }}
+        >
+          -10
+        </button>
+      </div>
+      <div className="counter-buttons">
+        <button
+          onClick={() => {
+            dispatch({ type: "multi", value: 2 });
+          }}
+        >
+          *2
         </button>
         <button
           onClick={() => {
-            dispatch((state) => {
-              return initialValue;
-            });
+            dispatch({ type: "multi", value: 10 });
+          }}
+        >
+          *10
+        </button>
+      </div>
+      <div className="counter-buttons">
+        <button
+          onClick={() => {
+            dispatch({ type: "divide", value: 2 });
+          }}
+        >
+          /2
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "divide", value: 10 });
+          }}
+        >
+          /10
+        </button>
+      </div>
+      <div className="counter-buttons">
+        <button
+          onClick={() => {
+            dispatch({ type: "reset" });
           }}
         >
           Reset
@@ -52,7 +111,7 @@ export default function Counter() {
       </div>
       <Info
         infos={[
-          "used useReducer() to increment,decrement and reset the count",
+          "used useReducer() to increment,decrement,,multiple,divide and reset the count",
           "used useContext to display the count on the another component (see on 'Counter' button on top)",
         ]}
       />
