a5873c69838a92443249e009b1d6bbca359e3591
ANAS <anaspappadan@gmail.com>
2024-07-09 11:59:37 +0530

    complete edit functionality to to do list

--- src/components/ReduxTodo.jsx
+++ src/components/ReduxTodo.jsx
@@ -41,26 +41,7 @@ export default function ReduxTodo() {
             <ul>
               {tasks.map((item, index) => (
                 <li key={item.id}>
-                  <span className="list-index">{index + 1} : </span>{" "}
-                  <span>{item.value}</span>
-                  <span
-                    className="task-edit"
-                    onClick={() => {
-                      dispatch(
-                        editTask({ index: index, newTask: "hello world" })
-                      );
-                    }}
-                  >
-                    Edit
-                  </span>
-                  <span
-                    className="task-delete"
-                    onClick={() => {
-                      dispatch(deleteTask(index));
-                    }}
-                  >
-                    Delete
-                  </span>
+                  <TaskDisplayField index={index} item={item} />
                 </li>
               ))}
             </ul>
@@ -68,5 +49,73 @@ export default function ReduxTodo() {
         )}
       </div>
     </div>
+  );
+}
+
+function TaskDisplayField({ item, index }) {
+  const [editOpen, setEditOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const [inputValue, setInputValue] = useState(item.value);
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(editTask({ index: index, newTask: inputValue }));
+    setEditOpen(false);
+  };
+  return (
+    <form
+      style={{
+        gap: "10px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <span className="list-index">{index + 1} : </span>{" "}
+        <input
+          onChange={handleChange}
+          className="taskField"
+          disabled={!editOpen}
+          type="text"
+          value={inputValue}
+        />
+        {editOpen && (
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="taskUpdateButton"
+          >
+            Update
+          </button>
+        )}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <span
+          className="task-edit"
+          onClick={() => {
+            setEditOpen((prev) => !prev);
+          }}
+        >
+          {editOpen ? "Cancel" : "Edit"}
+        </span>
+        <span
+          className="task-delete"
+          onClick={() => {
+            dispatch(deleteTask(index));
+          }}
+        >
+          Delete
+        </span>
+      </div>
+    </form>
   );
 }
